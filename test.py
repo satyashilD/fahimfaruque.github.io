af448eb295b863d5f8fa9b4f3d6bd7a1ca02e8b8
fahimfaruque <103014959+fahimfaruque@users.noreply.github.com>
2023-04-26 04:07:45 +0000

    asdad

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 
-url = "https://e3a4-20-124-214-182.ngrok-free.app/interactions"
+url = "https://78a9-20-124-214-182.ngrok-free.app/interactions"
 data = {"drug_list": ["aspirin","valsartan","lisinopril"]}
 
 response = requests.post(url, json=data)
